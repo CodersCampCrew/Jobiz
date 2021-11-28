519665ea06dbc802d252ea7b57161a5f00b15b3a
kostnerek <kostekmc@gmail.com>
2021-11-28 20:10:53 +0100

    fetched data from api and parsed 20 results

--- src/js/quzapiWrapper.js
+++ src/js/quzapiWrapper.js
@@ -4,21 +4,60 @@ import XMLHttpRequest from "xhr2";
 class QuizapiWrapper {
     constructor(category, difficulty) {
         this.apiKey = config.get('apiKey')
-        this.data="";
+        this.data;
         this.checkCategory(category) 
-        this.checkDifficulty(difficulty)
+        this.determineDifficulty(difficulty)
+        //this.data = this.getData();
+    };
 
+    determineDifficulty(difficulty) {
+        this.checkDifficulty(difficulty)
         if (this.difficulty != null) {
             this.url = `https://quizapi.io/api/v1/questions/?apiKey=${this.apiKey}&tags=${this.category}&difficulty=${this.difficulty}`;
         } else {
-            this.url = `https://quizapi.io/api/v1/questions/?apiKey=${this.apiKey}&tags=${this.category}&limit=1`;
+            this.url = `https://quizapi.io/api/v1/questions/?apiKey=${this.apiKey}&tags=${this.category}`;
         }
+    }
 
+    async getData() {
         
+        const callApi = (url) => {
+            const data = fetch(url)
+            .then(res => res.json())
+            .then(json => json)
+            .catch(err => console.error('Error:', err));
+            return data;
+        }
+        this.data = await callApi(this.url);
+        var allQuestions = [];
+        for (let i = 0; i < this.data.length; i++) {
+            var question = this.data[i]['question']
+            var answers = this.data[i]['answers']
+            var parsedAnswers = []
 
-        this.data = this.fetchApi();
-        //this.data = this.getData();
-    };
+            for (var key in answers) {
+                if(answers[key] != null) {
+                    parsedAnswers.push(answers[key])
+                }
+            }
+
+            var correct_answers = this.data[i]['correct_answers']
+            for (var key in correct_answers) {
+                if (correct_answers[key] == 'true') {
+                    let answer_key = key.substring(0, key.search("_correct"))
+                    var correct_answer = answers[answer_key]
+                }
+            }
+            allQuestions.push({
+                question: question,
+                answers: parsedAnswers.shuffle(),
+                correct_answer: correct_answer
+            });
+        }
+        console.log(allQuestions)
+        return allQuestions;
+    }
+
 
     checkCategory(category) {
         if (category == undefined) {
@@ -26,7 +65,7 @@ class QuizapiWrapper {
             return false;
         }
         category = category.toLowerCase()
-        const categories = ['linux', 'devops', 'networking', 'php', 'js', 'cloud', 'docker', 'kubernetes', 'python', 'c', 'java']
+        const categories = ['linux', 'devops', 'networking', 'php', 'javascript', 'cloud', 'docker', 'kubernetes', 'python']
         if ( !categories.includes(category) ) {
             throw new Error('Category out of range')
         } else {
@@ -47,15 +86,7 @@ class QuizapiWrapper {
         }
     }
 
-    async fetchApi() {
-        const data = await fetch(this.url)
-        .then(res => res.json())
-        .then(json => json)
-        .catch(err => console.error('Error:', err));
-        return await data;
-    }
-
 }
 
 let quizapiWrapper = new QuizapiWrapper('PHP')
-console.log(quizapiWrapper.data)
+console.log(quizapiWrapper.getData())
