e7ac7e7caf3f72be8fd07048505bd07147242018
kostnerek <kostekmc@gmail.com>
2021-12-27 00:01:05 +0100

    added logic to game

--- src/js/nickname.js
+++ src/js/nickname.js
@@ -1,6 +1,37 @@
+import { ls } from "./localstorage.js";
+import { QuizapiWrapper } from "./quizapiWrapper.js";
+async function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
 
-import {ls} from "./localstorage.js"
+    return array;
+}
 
-    const button = document.querySelector("button");
-        button.addEventListener("click", function () {
-        ls.setPlayerName(document.querySelector('input').value);});
+let quizapiWrapper = new QuizapiWrapper(ls.getPath());
+
+const button = document.querySelector("button");
+button.addEventListener("click", async function () {
+    const nickname = document.querySelector(".nickname-input");
+    if (nickname.value === "") {
+        nickname.style.border = "5px solid #d41c1c";
+        nickname.placeholder = "Please enter a nickname";
+    } else {
+        ls.setPlayerName(nickname.value);
+
+        document.querySelector(".lds-ring").style.display = "block";
+        button.style.display = "none";
+        ls.setPlayerName(document.querySelector("input").value);
+        const questions = await shuffle(await quizapiWrapper.getData());
+        ls.setQuestions(await questions);
+
+        window.location.href = "./question.html";
+    }
+});
--- src/js/questionPage.js
+++ src/js/questionPage.js
@@ -1,137 +1,187 @@
-// test array
-const questions = [
-	{
-		question: "Is JavaScript case-sensitive?",
-		answers: [
-			"True",
-			"False",
-			"True"
-		],
-		correct_answer: "True"
-	},
-	{
-		question: "Is JavaScript case-sensitive?(1)",
-		answers: [
-			"True(1)",
-			"False(1)"
-		],
-		correct_answer: "True"
-	},
-	{
-		question: "Is JavaScript case-sensitive?(2)",
-		answers: [
-			"True(2)",
-			"False(2)"
-		],
-		correct_answer: "True"
-	},
-]
-let questionCounter=0;
-let availableQuestions=[]; 
-let currentQuestion={};
-const answerElements = Array.from(document.getElementsByClassName("answer__button"));
+import { ProgressBar } from "./progressBar.js";
+import { ls } from "./localstorage.js";
 
-class ProgressBar {
-	constructor(seconds) {
-		this.secondsToCount = seconds;
-		this.countDownDate = new Date();
-		this.countDownDate.setSeconds(
-			this.countDownDate.getSeconds() + this.secondsToCount
-		);
-		this.createProgressBar();
-		this.element = document.getElementById("progress-bar-text");
+const _GAME_DATA = ls.getGame();
 
-		this.startBar();
-		this._status = true;
-	}
+class Game {
+    constructor(tier) {
+        this.tier = tier; // intern, junior etc...
+        const progressBarTimings = {
+            Intern: 30,
+            Junior: 20,
+            Regular: 15,
+            Senior: 5,
+        };
+        this.tierTimings = progressBarTimings[tier]; //determine how much time will be set on progress bar
+        this.questions = _GAME_DATA.questions; //get questions from local storage
+        this.currentQuestionIndex = _GAME_DATA.currentQuestion; // get current question index from local storage
+        this.currentQuestion = this.questions[this.currentQuestionIndex]; //set current question
 
-	createProgressBar() {
-		let progressBarElem = document.createElement("div");
-		progressBarElem.className = "round-time-bar";
-		progressBarElem.id = "progress";
-		progressBarElem.innerHTML =
-			"<div></div><p class='progress-bar-text' id='progress-bar-text'></p>";
+        this.progressQuestionBar = document.querySelector(".question-count"); //get questions progress bar
+        this.progressQuestionBar.value = this.currentQuestionIndex + 1; // set progress bar value to current question index
 
-		let healthBar = document.querySelector(".health");
-		let main = document.querySelector(".container");
-		main.insertBefore(progressBarElem, healthBar);
-		document.documentElement.style.setProperty("--duration", 15);
-	}
+        this.heart = ls.getPlayer().lives;
+        this.score = 0; //set score to 0
+        this.createQuestionPage();
+    }
 
-	startBar() {
-		const interval = setInterval(
-			(date = this.countDownDate, elem = this.element) => {
-				let now = new Date().getTime();
-				let distance = date - now;
-				let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-				elem.innerHTML = seconds + "s ";
-				if (distance < 0) {
-					clearInterval(interval);
-					elem.innerHTML = "You've lost heart";
-					this.status = false;
-					//TIMES UP
-				}
-			},
-			1000
-		);
-		this.element.innerHTML = this.secondsToCount + "s";
-	}
+    enableQuestions() {
+        document.getElementsByClassName("answer-container")[0].style.filter =
+            "blur(0px)";
+        document.getElementsByClassName(
+            "button-question-read"
+        )[0].style.visibility = "hidden";
+    }
+
+    disableQuestions() {
+        document.getElementsByClassName("answer-container")[0].style.filter =
+            "blur(5px)";
+        let btn = (document.getElementsByClassName(
+            "button-question-read"
+        )[0].style.visibility = "visible");
+    }
+
+    recreateAnswerContainer() {
+        /* delete all old answers and create container again */
+        const answerContainer = document.querySelector(".answer-container");
+        answerContainer.parentElement.removeChild(answerContainer);
+        const newAnswerContainer = document.createElement("section");
+        newAnswerContainer.className = "answer-container";
+        const main = document.querySelector(".container");
+        const btn = document.querySelector(".button-question-read");
+        main.insertBefore(newAnswerContainer, btn);
+    }
+
+    createQuestionPage() {
+        /* 
+			change question in question container
+			create answer buttons
+		*/
+        document.querySelector("#question").innerHTML =
+            this.currentQuestion.question;
+
+        for (let i = 0; i < this.currentQuestion.answers.length; i++) {
+            //create answer buttons
+
+            let answer = document.createElement("div");
+            answer.className = "answer";
+
+            let answerButton = document.createElement("button");
+            answerButton.className = "answer__button";
+            answerButton.setAttribute("data-number", i);
+            let answerText = this.currentQuestion.answers[i];
+            if (this.currentQuestion.correct_answer == answerText) {
+                this.correctAnswerButton = answerButton;
+            }
+
+            answerText = answerText.replace("<", "&lt;");
+            answerText = answerText.replace(">", "&gt;");
+            answerButton.innerHTML = answerText;
+            //protection against outputing html tags on site
+
+            answer.appendChild(answerButton);
+            document.querySelector(".answer-container").appendChild(answer);
+        }
+    }
+
+    nextQuestion() {
+        this.currentQuestionIndex++;
+        this.currentQuestion = this.questions[this.currentQuestionIndex];
+        this.progressQuestionBar.value = this.currentQuestionIndex + 1;
+
+        if (this.currentQuestionIndex == this.questions.length) {
+            ls.setCurrentScore(this.score);
+            window.location.href = "./score.html";
+        } else {
+            this.recreateAnswerContainer();
+            this.createQuestionPage();
+            this.disableQuestions();
+        }
+    }
+
+    correctAnswer(answer) {
+        /* 
+			kills progress bar
+			highlights correct answer
+			adds 1 to score
+			proceedes to next question
+		*/
+        this.pgBar.killBar();
+        answer.classList.add("answer__button--correct");
+        this.score++;
+        setTimeout(() => {
+            this.nextQuestion();
+        }, 2000);
+    }
+
+    wrongAnswer(answer = null) {
+        /* 
+			if wrong answer is given as argument it will be highlighted
+			if no argument is given it will be highlighted the correct answer only
+		*/
+        this.pgBar.killBar();
+        if (answer) {
+            answer.classList.add("answer__button--incorrect");
+        }
+        this.correctAnswerButton.classList.add("answer__button--correct");
+        setTimeout(() => {
+            this.nextQuestion();
+        }, 2000);
+    }
+
+    startGame() {
+        this.pgBar = new ProgressBar(this.tierTimings);
+        const answers = document.querySelectorAll(".answer__button");
+
+        answers.forEach((answer) => {
+            //adds event listener to answer buttons
+            answer.addEventListener("click", () => {
+                if (this.checkAnswer(answer.dataset.number)) {
+                    this.correctAnswer(answer);
+                } else {
+                    this.wrongAnswer(answer);
+                }
+            });
+        });
+
+        document
+            .getElementById("progress-bar-text")
+            .addEventListener("progressBarEnded", () => {
+                //called when progress bar ends
+                this.wrongAnswer();
+                return;
+            });
+    }
+
+    checkAnswer(answer) {
+        answer = this.currentQuestion.answers[answer];
+        if (answer == this.currentQuestion.correct_answer) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    deleteHeart() {
+        this.heart--;
+        const hearts = document.querySelectorAll(".fas");
+        hearts[this.heart].classList.add("icon--inactive");
+        if (this.heart == 0) {
+            console.log("game over");
+        }
+    }
 }
 
-function addHoverToAnswerButtons() {
-	let answerButtons = document.getElementsByClassName("answer__button");
-	for (let i = 0; i < answerButtons.length; i++) {
-		answerButtons[i].classList.add("answer__button-hover");
-	}
-}
-function enableQuestions() {
-	document.getElementsByClassName("answer-container")[0].style.filter =
-		"blur(0px)";
-	document.getElementsByClassName("button-question-read")[0].style.visibility = "hidden";
-}
-
-function disableQuestions() {
-	document.getElementsByClassName("answer-container")[0].style.filter = "blur(5px)";
-	let btn = document.getElementsByClassName("button-question-read")[0].style.visibility = "visible";
-}
-
-function startQuiz(){
-questionCounter=0;
-availableQuestions=[...questions];
-console.log(availableQuestions);
-newQuestion();
-}
-
-function newQuestion(){
-questionCounter++;
-const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-currentQuestion = availableQuestions[questionIndex];
-question.innerHTML = currentQuestion.question;
-
-answerElements.forEach(answer =>{
-	const number= answer.dataset['number'];
-	answer.innerHTML = currentQuestion.answers[number - 1];
-})
-
-availableQuestions.splice(questionIndex, 1);
-
-}
-answerElements.forEach(answer => {
-answer.addEventListener("click", e=>{
-	newQuestion();
-	disableQuestions()
-})
-})
+const game = new Game(_GAME_DATA.tier);
 
 window.onload = function () {
-	document
-		.getElementById("button-question-read")
-		.addEventListener("click", () => {
-			enableQuestions();
-			addHoverToAnswerButtons();
-			new ProgressBar(15);
-		});
+    game.deleteHeart();
+    game.deleteHeart();
+    game.deleteHeart();
+    document
+        .getElementById("button-question-read")
+        .addEventListener("click", () => {
+            game.enableQuestions();
+            game.startGame();
+        });
 };
-
-
-startQuiz(); //Function Test
